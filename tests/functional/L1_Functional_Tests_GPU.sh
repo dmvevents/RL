77dd3aad2f9ed0f3de579f7095da86d1bedaafba
Virginia Wu <vadams@nvidia.com>
2026-02-09 18:47:50 -0800

    added tests

--- tests/functional/L1_Functional_Tests_GPU.sh
+++ tests/functional/L1_Functional_Tests_GPU.sh
@@ -37,6 +37,9 @@ time uv run --no-sync bash ./tests/functional/grpo_automodel_lora_async.sh
 time uv run --no-sync bash ./tests/functional/grpo_automodel_lora_non_colocated.sh
 time uv run --no-sync bash ./tests/functional/grpo_megatron.sh
 time uv run --no-sync bash ./tests/functional/grpo_megatron_generation.sh
+time uv run --no-sync bash ./tests/functional/grpo_megatron_lora.sh
+time uv run --no-sync bash ./tests/functional/grpo_megatron_lora_async.sh
+time uv run --no-sync bash ./tests/functional/grpo_megatron_lora_non_colocated.sh
 time uv run --no-sync bash ./tests/functional/grpo_multiple_datasets.sh
 time uv run --no-sync bash ./tests/functional/grpo_multiturn.sh
 time uv run --no-sync bash ./tests/functional/grpo_non_colocated.sh
